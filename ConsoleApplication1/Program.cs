f03ab8248cd15c163969d6d33c02e0f877aed4b1
byrney <robert@byrnemail.org>
2014-09-07 22:32:26 +0100

    Event Stream stays open using TaskCompletionSource

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,12 +11,11 @@ using System.Collections.Generic;
 
 namespace OwinConsole
 {
+    // runs the server
     class Program
     {
         static void Main(string[] args)
         {
-
-            StartOptions options = new StartOptions();
             using (WebApp.Start<Startup>("http://localhost:12345"))
             {
                 Console.ReadLine();
@@ -24,52 +23,92 @@ namespace OwinConsole
         }
     }
 
-    public class Api
-    {
-        System.Timers.Timer _timer = new System.Timers.Timer(25000);
-        List<StreamWriter> _writers = new List<StreamWriter>();
-        public Api()
-        {
-            _timer.Elapsed += _timer_Elapsed;
-         //   _timer.Start();
-        }
-
-        void _timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
-        {
-            UpdateWriters();
-        }
-
-        public void UpdateWriters()
-        {
-
-            _writers.ForEach(w => w.WriteLineAsync("Hello async"));
-            _timer.Start();
-
-        }
-
-        public async Task Invoke(IOwinContext context)
-        {
-            context.Response.ContentType = "text/eventstream";
-            context.Response.Headers["Content-Encoding"] = "chunked";
-            context.Response.Headers["cache-control"] = "no-cache";
-            context.Response.ContentLength = null;
-            System.IO.Stream responseStream = context.Environment["owin.ResponseBody"] as Stream;
-            var w = new StreamWriter(responseStream);
-            _writers.Add(w);
-            await w.WriteLineAsync("Registered");
-            context.Response.Body.Flush();
-            _timer.Start();
-         //  w.Close();
-          //  await context.Response.WriteAsync("Hello from Api");
-        }
-    }
-
+    // creates the pipeline  (of one)
     public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
             var api = new Api();
             app.Run(context => api.Invoke(context));
+        }
+    }
+
+    public class Subscriber
+    {
+        private StreamWriter _writer;
+        private TaskCompletionSource<bool> _tcs;
+        public Subscriber(Stream body, TaskCompletionSource<bool> tcs)
+        {
+            this._writer = new StreamWriter(body);
+            this._tcs = tcs;
+        }
+
+        public async void WriteAsync(string message)
+        {
+            try
+            {
+                _writer.Write(message);
+                _writer.Flush();
+            }
+            catch(Exception e)
+            {
+                if (e.HResult == -2146232800) // non-existent connection
+                    _tcs.SetResult(true);
+                else
+                    _tcs.SetException(e);
+            }
+        }
+    }
+
+    public class Api
+    {
+        System.Timers.Timer _timer = new System.Timers.Timer(10000);
+        List<Subscriber> _subscribers = new List<Subscriber>();
+        public Api()
+        {
+            _timer.Elapsed += _timer_Elapsed;
+        }
+
+        void _timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            UpdateSubscribers();
+        }
+
+        public void UpdateSubscribers()
+        {
+            Console.WriteLine("updating {0} subscribers", _subscribers.Count);
+            var subscribersCopy = _subscribers.ToList<Subscriber>();
+            var msg = String.Format("Hello async at {0}\n", DateTime.Now);
+            subscribersCopy.ForEach(w => w.WriteAsync(msg));
+            _timer.Start();
+        }
+
+
+        public Task Invoke(IOwinContext context)
+        {
+            SetEventHeaders(context);
+            System.IO.Stream responseStream = context.Environment["owin.ResponseBody"] as Stream;
+            var tcs = new TaskCompletionSource<bool>();
+            var s = CreateSubscriber(responseStream, tcs);
+            tcs.Task.ContinueWith(_ => _subscribers.Remove(s));
+            Console.WriteLine("Add subscriber. Now have {0}", _subscribers.Count);
+            s.WriteAsync("Registered\n");
+            _timer.Start();
+            return tcs.Task;            
+        }
+
+        private Subscriber CreateSubscriber(System.IO.Stream responseStream, TaskCompletionSource<bool> tcs)
+        {
+            var s = new Subscriber(responseStream, tcs);
+            _subscribers.Add(s);
+            return s;
+        }
+
+        private static void SetEventHeaders(IOwinContext context)
+        {
+            context.Response.ContentType = "text/eventstream";
+            context.Response.Headers["Transfer-Encoding"] = "chunked";
+            context.Response.Headers["cache-control"] = "no-cache";
         }
     }
 
